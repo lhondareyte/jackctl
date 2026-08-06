9b02ddb6fc197a33d4ffa9f7a9690ecae802f3f5
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2026-08-06 17:03:49 +0200

    Switch to Perl for regression tests

---
+++ tests/regression.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my %messages = (
+	"test_1.ini" => 'Warning : Missing source in "Missing source keyword" section',
+	"test_2.ini" => 'Warning : Missing destination in "Missing destination keyword" section',
+	"test_3.ini" => "Error:   test_3.ini[4]: Unknown action \"src\"\nWarning : Missing source in \"Wrong keyword\" section",
+	"test_4.ini" => 'Error:   test_4.ini[4]: Invalid section name',
+	"test_5.ini" => 'Error: cannot connect foo to bar'
+);
+
+my @confiles = (
+	"test_1.ini", "test_2.ini", "test_3.ini", 
+	"test_4.ini", "test_5.ini", "test_6.ini" 
+);
+
+foreach my $file (@confiles) {
+	if ( -f $file ) {
+		my $msg = `../jackctl -v -f $file 2>&1`;
+		if ( not defined $messages{$file} ) {
+			next;
+		}
+		chomp $msg;
+		if ( $msg ne $messages{$file} ) {
+			die "Regression with $file configuration file\n";
+		}
+	}
+}
+print STDOUT "All tests passed.\n"
